de12d13b7314dbbef3caf78e7cb7b720f1bebb3d
Brian LaMattina <brianlamattina@gmail.com>
2021-08-29 21:02:50 -0400

    Add plugins for yaml frontmatter

--- src/vault-tasks/tokenizer.ts
+++ src/vault-tasks/tokenizer.ts
@@ -1,18 +1,28 @@
 import { fromMarkdown as mdastFromMarkdown } from "mdast-util-from-markdown";
 import { toMarkdown as mdastToMarkdown } from "mdast-util-to-markdown";
 import { gfmTaskListItem } from "micromark-extension-gfm-task-list-item";
+import { frontmatter } from "micromark-extension-frontmatter";
 import {
   gfmTaskListItemFromMarkdown,
   gfmTaskListItemToMarkdown,
 } from "mdast-util-gfm-task-list-item";
+import {
+  frontmatterFromMarkdown,
+  frontmatterToMarkdown,
+} from "mdast-util-frontmatter";
 
 export const fromMarkdown = function (markdown: string) {
   return mdastFromMarkdown(markdown, {
-    extensions: [gfmTaskListItem],
-    mdastExtensions: [gfmTaskListItemFromMarkdown],
+    extensions: [gfmTaskListItem, frontmatter(["yaml"])],
+    mdastExtensions: [
+      gfmTaskListItemFromMarkdown,
+      frontmatterFromMarkdown(["yaml"]),
+    ],
   });
 };
 
 export const toMarkdown = function (tree: object) {
-  return mdastToMarkdown(tree, { extensions: [gfmTaskListItemToMarkdown] });
+  return mdastToMarkdown(tree, {
+    extensions: [gfmTaskListItemToMarkdown, frontmatterToMarkdown(["yaml"])],
+  });
 };
